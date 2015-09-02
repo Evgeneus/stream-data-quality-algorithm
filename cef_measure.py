fea156fe070bc0431ca79f33a6bd436c63ed7389
Evgeny <krivosheevevgeny@gmail.com>
2015-09-02 22:16:36 +0300

    initial commit

---
+++ cef_measure.py
@@ -0,0 +1,20 @@
+def get_CEF(O, S):
+    N = len(O)
+    for i in range(N-1):
+        print i, 'mis-capturable'
+        if O[i] != S[i]:
+            print i, 'capturable'
+            if O[i+1] == S[i+1]:
+                print i, 'captured'
+        if O[i] != S[i] and O[i+1] != S[i+1]:
+            print i, 'mis-captured'
+
+    if O[N-1] != S[N-1]:
+        print N-1, 'capturable'
+        print N-1, 'mis-capturable'
+
+if __name__ == '__main__':
+    ground_truth = ['Wisc','Wisc','Wisc','Wisc','MSR']
+    observed =     [None  , 'UW' , None ,'Wisc','Wisc']
+    
+    get_CEF(ground_truth, observed)
